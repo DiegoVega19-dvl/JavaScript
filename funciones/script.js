86ff9b5b70e8e03792d2e6d611eafead772eb0e9
Juan Diego <diegovegaprogra@gmail.com>
2026-04-29 13:36:17 -0700

    -- retomando funciones --

--- funciones/script.js
+++ funciones/script.js
@@ -3,7 +3,7 @@
 }
 saludar();*/
 
-function suma(num1, num2) {
+/*function suma(num1, num2) {
   let resultado;
 
   resultado = num1 + num2;
@@ -15,4 +15,55 @@ let sumar = suma(1, 2);
 console.log("el resultado de la suma es: " + sumar);
 
 sumar = suma(10, 2);
-console.log("el resultado de la suma es: " + sumar);
+console.log("el resultado de la suma es: " + sumar);*/
+
+// funciones con parametros
+
+/*function suma(num, num2) {
+  return num + num2;
+}*/
+
+//console.log(suma(1, 2));
+
+// funciones rest (con parametros indefinidos)
+
+/*function multiplicacion(...args) {
+  let total = 1;
+  for (let a of args) {
+    total *= a;
+  }
+  return total;
+}
+
+console.log(multiplicacion(2, 2, 2, 4));*/
+
+// funciones flecha
+
+/*let suma = (a, b) => a + b;
+
+console.log(suma(1, 2));*/
+
+/*function suma(a, b) {
+  return a + b;
+}*/
+
+/*let multiplicacion = (...args) => {
+  let res = 1;
+  for (let a of args) {
+    res *= a;
+  }
+  return res;
+};
+
+console.log(multiplicacion(1, 2, 3, 4, 5, 6));*/
+
+/*let suma = (a, b) => a + b;
+console.log(suma(1, 2));*/
+
+// funciones IIFE ------------------------------------------------->
+
+/*(() => {
+  console.log("hola");
+})();*/
+
+// funciones de orden superior -------------------------->
